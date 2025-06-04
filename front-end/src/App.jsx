6fbcfc0b0fc9127f7bf9e471a9653275533f159b
Zsofie <zsofia@linumlabs.com>
2025-06-04 16:13:51 -0700

    added wallet display component

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,16 +1,17 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./components/pages/Login";
+import CreatorDashboard from "./components/pages/CreatorDashboard";
 
 const App = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Login />} />
-        <Route path="/creator" element={<div>Creator Page</div>} />
+        <Route path="/creator" element={<CreatorDashboard />} />
       </Routes>
     </Router>
   );
 };
 
-export default App; 
+export default App;
